d4fc73da1b943014c28382531993792bba4c6184
notarealwizard@gmail.com <notarealwizard@gmail.com>
2021-01-09 12:56:25 -0600

    Resto Druid Module updated for Shadowlands

--- Parsers/RestoDruid.lua
+++ Parsers/RestoDruid.lua
@@ -64,19 +64,37 @@ local function hotCount(unit)
 	return count;
 end
 
+local function hasRegrowthHoT(unit) 
+	local count = 0;
+	for i=1,40,1 do
+		local _,_,_,_,_,_,p,_,_,id = UnitAura(unit,i);
+
+		if ( not id ) then
+			break;
+		elseif (p == "player" and id == addon.Druid.Regrowth) then
+			return true;
+		end
+	end
+	return false;
+end
+
 
 
 --[[----------------------------------------------------------------------------
 	Druid Critical Strike 
 		- modified by abundance on regrowth
+		- modified by rank2 regrowth
 ------------------------------------------------------------------------------]]
 local function _CriticalStrike(ev,spellInfo,heal,destUnit,C,CB)
 	
 	if ( spellInfo.spellID == addon.Druid.Regrowth ) then
 		local abundance = addon.BuffTracker:Get(addon.Druid.AbundanceBuff);
 		C = C + (abundance * 0.06);
-	
-		--TOOD: on regrowth initial heal, check if regrowth HoT already exists on the target and add 40% increased crit chance
+
+		--check for initial hit of regrowth on an existing regrowth HoT
+		if ( ev ~= "SPELL_PERIODIC_HEAL" and hasRegrowthHoT(destUnit) ) then
+			C = C + 0.40; --rank2 regrowth
+		end
 	end
 	
 	return addon.BaseParsers.CriticalStrike(ev,spellInfo,heal,destUnit,C,CB,nil);
@@ -87,19 +105,30 @@ end
 --[[----------------------------------------------------------------------------
 	Druid Mastery 
 		- modified by hotcount on target
+		- amplified when using Nourish
+		- frenzied regen not affected by it's own mastery stack
 ------------------------------------------------------------------------------]]
 local DruidArtifactWeaponID = 128306;
 
 local function _Mastery(ev,spellInfo,heal,destUnit,M)
 	if ( spellInfo.mst ) then --spell is affected by mastery, get the hotCount on target.		
+		--HotCount
 		local count = hotCount(destUnit);
 		
-		if ( spellInfo.spellID == addon.Druid.FrenziedRegen and not IsEquippedItem(DruidArtifactWeaponID) ) then --FR isnt affected by itself when not using artifact weapon https://imgur.com/SmHa1bw
-			count = math.max(0,count-1);
-		end
-		
+		--Nourish
 		if ( spellInfo.spellID == addon.Druid.Nourish ) then
 			count = count * 3;
+		end
+
+		--Frenzied Regen:
+		--[[ 
+			Frenzied regen is a "HoT" that boosts the mastery of other spells,
+			Frenzied regen healing is increased by druid mastery,
+			BUT frenzied regen is not increased by it's own mastery HoT (1-9-2021)
+			This is the only druid spell I have found which works this way.
+		--]]
+		if ( spellInfo.spellID == addon.Druid.FrenziedRegen ) then 
+			count = math.max(0,count-1);
 		end
 
 		return count*heal / (1+count*M) / addon.MasteryConv;
